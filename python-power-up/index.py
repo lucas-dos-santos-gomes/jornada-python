ef824df2993b09e2d819f0bb2eecf8e69ba9205f
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-01-21 16:15:48 -0300

    Power Up - Acesso a página de cadastro

--- python-power-up/index.py
+++ python-power-up/index.py
@@ -1,5 +1,22 @@
+# RPA = Robotic Process Automation (Automação Robótica de Processos)
 import pyautogui as pag
+import time
+
+pag.PAUSE = 1.5
 
 pag.press("win")
 pag.write("chrome")
+pag.press("enter")
+
+time.sleep(2)
+
+pag.click(x=247, y=644)
+pag.hotkey("win", "up")
+pag.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pag.press("enter")
+
+pag.press("tab")
+pag.write("pythonimpressionador@gmail.com")
+pag.press("tab")
+pag.write("python2024")
 pag.press("enter")
